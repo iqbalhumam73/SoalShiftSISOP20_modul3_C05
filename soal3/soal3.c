8ed0838f7d68041b490f66d62c0120c86f3388dc
ohmyjons <dohanpw@gmail.com>
2020-04-11 18:07:38 +0700

    -d

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -4,9 +4,19 @@
 #include<unistd.h>
 #include<pthread.h>
 #include<sys/wait.h>
+#include<dirent.h>
+#include<unistd.h>
+#include<math.h>
+#include<ctype.h>
+#include<errno.h>
 #include<sys/stat.h>
 
 #define SIZE 200
+
+
+char huruf[SIZE], current_dir[SIZE];
+char *arr[4] ,*arr2[20], arr3[SIZE]; //arr menyimpan ekxstensi arr2 menyimpan nama file
+int a=0,b=0;
 
 int cek_slash(char dir[]){
 	int n = strlen(dir);
@@ -39,7 +49,9 @@ void* makedir(void *ext){
     strcat(dir,ext);
     printf("%s\n",dir);
     int result = mkdir(dir,0700);
-} 
+}
+
+void* makeit(void *);
 
 // void* movefile()
 
@@ -59,23 +71,32 @@ int cekfile(char dir[]){
 int main(int argc, char **argv){
     
     const char ext[SIZE];
-
+    pthread_t tid[argc-2];
     char save[argc];
+    int i=0,j=0;
+
+    if (getcwd(current_dir, sizeof(current_dir))!=NULL) printf("Directory sekarang : %s\n" , current_dir);
+
+    
     //get argument
     if ( strcmp(argv[1],"-f")== 0)
     {
-        printf("oyi\n");
+        printf("Masuk ke -f\n");
+        for( j=2;j<argc;j++){
+            pthread_create(&(tid[i]), NULL, makeit, argv[j]);
+            pthread_join(tid[i], NULL);
+            i++;
+        }   
     }
     else if (strcmp(argv[1],"-d")== 0)
     {
-        /* code */
+        
     }
     else if (strcmp(argv[1],"*")== 0)
     {
         /* code */
     }
     
-    pthread_t tid[argc-2];
     
     //get file ext
     for (int i = 2; i < argc; i++)
@@ -98,10 +119,57 @@ int main(int argc, char **argv){
 }
 
 
+void* makeit(void *arg)
+{
+    //copy string ke variabel huruf
+    // int a = 0, b = 0;
+    int i=0;
+    strcpy(huruf,arg);
+    char *tanda, *tanda1;
+    tanda1 = strtok(huruf, "/"); //memisahkan string dari karakter (huruf) dengan / sebagai titik pemisahan
+    while( tanda1 != NULL ) {
+        //membaca string
+        arr2[b] = tanda1; b++;
+        tanda1 = strtok(NULL, "/");
+    }
+    strcpy(arr3, arr2[b-1]);
+    tanda = strtok(arr2[b-1], "."); //memisahkan string dari karakter (arr) dengan . sebagai titik pemisahan
+    while( tanda != NULL ) {
+        //membaca string
+        arr[a] = tanda; a++;
+        tanda = strtok(NULL, "."); //memisahkan string dengan . sebagai titik pemisahan
+    }
 
+    char abc[100];
+    strcpy(abc,arr[a-1]);
+    for(i = 0; abc[i]; i++) abc[i] = tolower(abc[i]); //convert menjadi lowercase letter
 
-void _print(){
+    char place1[100],place2[100];
 
+    //menyusun direktori lokasi file
+    strcpy(place1,current_dir);
+    strcat(place1,"/");
+    strcat(place1,abc);
+    //memberitahu lokasi file
+    printf("Berada di = %s\n%s\n",abc,place1);
+    printf("SELESAI\n");
+    mkdir(place1, 0777);
+    
+	
 
-
+    char sumber[1024], tujuan[1024];
+    //lokasi file
+    strcpy(sumber,arg);
+    strcpy(tujuan,current_dir);
+    strcat(tujuan,"/");
+    
+    //untuk move file dengan rename
+    strcat(tujuan, abc);
+    strcat(tujuan,"/");
+    strcat(tujuan,arr3);
+    rename(sumber,tujuan);
+    a = 0;
+    b = 0;
+	return NULL;
 }
+
